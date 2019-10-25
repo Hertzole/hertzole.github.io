66e6adff6fe62bd89f3e37350db515b40a1f5e5b
Hampus Ringström <hampusringstrom@gmail.com>
2019-10-25 14:14:08 +0200

    Image updates.
    Converted to jpgs to decrease side.
    Fixed style not being loaded on 404 page.

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ function applyRandomImage() {
         "/images/backgrounds/sj_1.png",
         "/images/backgrounds/sj_2.png",
         "/images/backgrounds/sj_3.png",
-        "/images/backgrounds/jumps_1.png",
-        "/images/backgrounds/jumps_2.png",
-        "/images/backgrounds/jumps_3.png",
+        "/images/backgrounds/jumps_1.jpg",
+        "/images/backgrounds/jumps_2.jpg",
+        "/images/backgrounds/jumps_3.jpg",
         "/images/backgrounds/qs_1.jpg",
         "/images/backgrounds/qs_2.jpg",
         "/images/backgrounds/qs_3.jpg",
-        "/images/backgrounds/sh_1.png",
-        "/images/backgrounds/sh_2.png",
-        "/images/backgrounds/sh_3.png",
+        "/images/backgrounds/sh_1.jpg",
+        "/images/backgrounds/sh_2.jpg",
+        "/images/backgrounds/sh_3.jpg",
     ];
     backgroundElement.style.backgroundImage = "url('" + images[Math.floor(Math.random() * images.length)] + "')";
 }
